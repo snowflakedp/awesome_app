4f093a00c753f387cd2203cc3460341c30e5f6e0
snowflakedp <110241849+snowflakedp@users.noreply.github.com>
2022-09-28 11:13:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # reads in csv and puts into variable my_fruit_list
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_set = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_set.index))
+my_fruit_list = my_fruit_set["fruit"]
 
 # displays dataframe
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_set)
